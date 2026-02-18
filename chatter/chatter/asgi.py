da113a47a756ccbb5031329fdb0d7047806bf157
roman <romantiholiz154@gmail.com>
2026-02-18 09:03:55 +0200

    feat: consumer added

--- chatter/chatter/asgi.py
+++ chatter/chatter/asgi.py
@@ -8,13 +8,18 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from chat_app.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatter.settings')
 
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    'http': django_asgi_app
+    'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
 })
