e64ff04df57a84ef5d189fc5cd2cd232281d9b0e
Oleh Baron <olehrik2009@gmail.com>
2026-02-18 10:52:19 +0200

    B;allasd

--- chatter/chat_app/consumers.py
+++ chatter/chat_app/consumers.py
@@ -1,13 +1,25 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         print("WebSocket connected")
+        self.id = 1 #self.scope['url_route']['kwargs']['course_id']
+        self.room_group_name = f'chat_{self.id}'
+        # join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
         # accept connection
         self.accept()
 
+
     def disconnect(self, close_code):
+        # leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
         print("WebSocket disconnected")
 
     # receive message from WebSocket
@@ -18,3 +30,15 @@ class ChatConsumer(WebsocketConsumer):
         # send message to WebSocket
         self.send(text_data=json.dumps({'message': message}))
         print("Sent message: " + message)
+        async_to_sync(self.channel_layer.group_send)(
+        self.room_group_name,
+        {
+        'type': 'chat_message',
+        'message': message,
+        }
+        )
+
+    # receive message from room group
+    def chat_message(self, event):
+        # send message to WebSocket
+        self.send(text_data=json.dumps(event))
