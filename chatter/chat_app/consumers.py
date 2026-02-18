78ae86468e8b38a3c93ff54bd40e31bbae297fc7
roman <romantiholiz154@gmail.com>
2026-02-18 09:26:39 +0200

    commit

--- chatter/chat_app/consumers.py
+++ chatter/chat_app/consumers.py
@@ -3,16 +3,17 @@ from channels.generic.websocket import WebsocketConsumer
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        print("WebSocket connected")
         # accept connection
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        print("WebSocket disconnected")
 
     # receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        
+
         # send message to WebSocket
         self.send(text_data=json.dumps({'message': message}))
--- chatter/chatter/settings.py
+++ chatter/chatter/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chat_app',
-    'daphne',
 ]
 
 MIDDLEWARE = [
