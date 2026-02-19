52a7d2784e64a74a4f2f9884167b7c3d6cb99174
Pavlo <pavlosirba974@gmail.com>
2026-02-19 10:46:34 +0200

    Added websocket's channels

--- chatter/chat_app/consumers.py
+++ chatter/chat_app/consumers.py
@@ -1,19 +1,37 @@
 import json
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
 class ChatConsumer(WebsocketConsumer):
 
     def connect(self):
-    # accept connection
+        self.room_group_name = f'chat_1'
+        
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
+
         self.accept()
 
     def disconnect(self, close_code):
-        pass
-        # receive message from WebSocket
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
+ 
         
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         print(f"received {message}")
+        
+        async_to_sync(self.channel_layer.group_send)(
+        self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+            }
+        )
+
+    def chat_message(self, event):
         # send message to WebSocket
-        self.send(text_data=json.dumps({'message': message}))
+        self.send(text_data=json.dumps(event))
